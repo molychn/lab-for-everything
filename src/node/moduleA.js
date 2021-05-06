b5a7d00f37b733a7a95666fc487a556fc4f9521b
molychn <15766227859@163.com>
2021-05-06 14:51:21 +0800

    node module require

---
+++ src/node/moduleA.js
@@ -0,0 +1,12 @@
+const a = 1;
+
+function foo () {
+  console.log(a)
+}
+
+// module.exports = {
+//   a,
+//   foo
+// }
+
+exports.foo = foo
---
+++ src/node/moduleB.js
@@ -0,0 +1,6 @@
+const moduleA = require('./moduleA');
+
+// console.log(moduleA.a);
+// moduleA.a = 2;
+// console.log(moduleA.a);
+moduleA.foo();
